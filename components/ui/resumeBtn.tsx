e69416c4464c573104a782c44065f417a2c362fd
Jain Kuriakose <kuriakosejain456@gmail.com>
2024-10-24 20:20:16 +0530

    resume downlaod btn completed

--- components/ui/resumeBtn.tsx
+++ components/ui/resumeBtn.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
+import { RainbowButton } from './rainbow-button';
 
 const ResumeButton = () => {
   const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = '/utils/Your_Resume.pdf'; // Update with the actual path to your resume
-    link.download = 'Resume.pdf'; // Update with the desired file name
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    window.open('https://drive.google.com/file/d/1Ik3KFP4PlqH1Afop310iuXi2C3S0sVSe/view?usp=sharing', '_blank');
   };
 
   return (
-    <button 
+    <RainbowButton 
       onClick={handleDownload} 
       className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700"
     >
       Download My Resume
-    </button>
+    </RainbowButton>
   );
 };
 
