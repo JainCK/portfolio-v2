b331a5dd2f02131815d72a29fc5f86e643aefe16
Jain Kuriakose <kuriakosejain456@gmail.com>
2024-10-24 23:45:15 +0530

    init

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     env: {
         GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
       },
+      output: "export",
+      reactStrictMode: true,
 };
 
 export default nextConfig;
