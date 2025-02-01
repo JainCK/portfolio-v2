7464661d8bb860ea44483d8be56c42000ac546f2
Jain Kuriakose <kuriakosejain456@gmail.com>
2025-02-01 13:29:22 +0530

    font changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,24 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { ApolloWrapper } from "@/components/apolloWrapper";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "Welcome to my Portfolio",
-  description: "this is a portfolio page for Jain C Kuriakose, Full-stack developer",
+  description:
+    "this is a portfolio page for Jain C Kuriakose, Full-stack developer",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-950 overflow-y-scroll overflow-x-hidden px-4 sm:px-24`}
+        className={`antialiased bg-white overflow-y-scroll overflow-x-hidden px-4 sm:px-24`}
       >
         <ApolloWrapper>
           <Navbar />
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React from "react";
 import { FaGithub } from "react-icons/fa";
-import { PiTwitterLogoFill } from 'react-icons/pi';
+import { PiTwitterLogoFill } from "react-icons/pi";
 import { FaLinkedinIn } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
-    <nav className="px-24 py-10 flex justify-between items-center fixed top-0 left-0 right-0 z-50 text-white">
+    <nav className="anton-regular px-24 py-10 flex justify-between items-center fixed top-0 left-0 right-0 z-50 text-black">
       <div className="text-2xl font-bold">@jain</div>
       <ul className="hidden md:flex space-x-4">
-        <li><a href="#projects" className="hover:underline">Projects</a></li>
-        <li><a href="#skills" className="hover:underline">Skills</a></li>
-        <li><a href="#about" className="hover:underline">About</a></li>
+        <li>
+          <a href="#projects" className="hover:underline">
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#skills" className="hover:underline">
+            Skills
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="hover:underline">
+            About
+          </a>
+        </li>
       </ul>
       <div className="flex space-x-2 text-xl md:text-2xl">
-        <a href="https://twitter.com/jain_kuriakose" target="_blank" rel="noopener noreferrer">
-        <PiTwitterLogoFill />
+        <a
+          href="https://twitter.com/jain_kuriakose"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <PiTwitterLogoFill />
         </a>
-        <a href="https://www.linkedin.com/in/jainck" target="_blank" rel="noopener noreferrer">
-        <FaLinkedinIn />
+        <a
+          href="https://www.linkedin.com/in/jainck"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedinIn />
         </a>
-        <a href="https://github.com/JainCK" target="_blank" rel="noopener noreferrer">
-        <FaGithub/ >
+        <a
+          href="https://github.com/JainCK"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
         </a>
       </div>
     </nav>
