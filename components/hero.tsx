ae63f77ecba469e3e02022aced473c392ffe9232
Jain Kuriakose <kuriakosejain456@gmail.com>
2025-02-01 13:29:52 +0530

    particle file fix and hero style change

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,40 +1,33 @@
 "use client";
 
-import Particles from '@/components/ui/particles';
-import { motion } from 'framer-motion';
-import HyperText from './ui/hyper-text';
+import Particles from "@/components/ui/particles";
+import { motion } from "framer-motion";
 
 export const Hero = () => {
-  const color = "#ffffff"
+  const color = "#ffffff";
 
   return (
-
-    <div className="relative flex flex-col h-screen justify-center bg-slate-950 overflow-hidden">
+    <div className="relative flex flex-col h-screen justify-center bg-white overflow-hidden">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 3 }}
         className="text-center"
       >
-        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-white to-blue-500 text-left pl-10">
-          Hello,<br />
-          I&apos;m Jain, <br />
-          <HyperText
-      className="text-3xl font-bold text-white"
-      text="Full-Stack Developer"
-    />
-          
+        <h1 className="anton-regular text-6xl sm:text-4xl md:text-8xl text-black text-left pl-10">
+          Hi, I'm Jain
         </h1>
-        <p className="text-lg sm:text-md md:text-lg mt-1 font-light text-gray-300 text-left pl-10">
-          Passionate about crafting interactive digital experiences for web and app platforms, based in India 
+        <p className="anton-regular text-lg sm:text-md md:text-6xl mt-1 font-light text-black text-left pl-10">
+          Full-Stack Developer in India.
+          <br /> Let's build something amazing.
         </p>
-        <div className='pl-10'></div>
-        </motion.div>
-        <Particles
+        <div className="pl-10"></div>
+      </motion.div>
+      <Particles
         className="absolute inset-0"
         quantity={500}
         ease={80}
-        color={color}
+        color={"#000000"}
         refresh
       />
     </div>
